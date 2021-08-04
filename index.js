bc056611e42d9a7e8e435b2430ed670533c22814
Ninad Sonawane <ninadsonawane@Ninads-MacBook-Air.local>
2021-08-04 20:53:32 +0530

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ limit:"30mb" , extended: true }));
 app.use(cors());
 
 app.use('/reviews' , routes);
-
+ 
 const PORT = process.env.PORT;
 
 app.get('/' , (req , res) =>  { 
